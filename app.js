283fc2fa2adabf90b9fcdd56b369bad0d9fd3f37
LiuTianhang <E0175471@u.nus.edu>
2019-02-09 23:44:07 +0800

    helo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var methodOR = require('method-override');
+var methodOR = require('method-override');
 /* --- V7: Using dotenv     --- */
 require('dotenv').load();
 
@@ -42,7 +42,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(methodOR('_method'));
+app.use(methodOR('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
